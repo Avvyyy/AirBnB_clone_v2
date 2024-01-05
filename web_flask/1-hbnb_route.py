0525fac8a1511f903d91331bcb2a6852dde3678b
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2024-01-05 00:45:58 +0000

    Creation of web_flask/1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,31 @@
+#1/usr/bin/pyhton3
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def home():
+    """
+    A home route in flask
+    
+    Arguments:
+    None
+
+    Return:
+    Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    hbnb route
+    
+    Arguments:
+    None
+
+    Return:
+    HBNB
+    """
+    return "HBNB"
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
