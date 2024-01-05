392989b4c613d5fe6b79f8afd10e44d15b181785
Favour Uzochukwu <uzochukwu.favour310@gmail.com>
2024-01-05 01:43:05 +0000

    Minor Fix

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,49 +1,27 @@
 #!/usr/bin/python3
-"""Defines routes"""
-from flask import Flask, render_template
+"""Script that starts a Flask web application"""
+
+from flask import Flask
+from markupsafe import escape
+
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
-def home():
-    """
-    A home route in flask
-    
-    Arguments:
-    None
 
-    Return:
-    Hello HBNB!
-    """
-    return"Hello HBNB!"
+@app.route("/", strict_slashes=False)
+def display_hello():
+    return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """
-    hbnb route
-    
-    Arguments:
-    None
-
-    Return:
-    HBNB
-    """
+def desplay_hbnb():
     return "HBNB"
 
+
 @app.route("/c/<text>", strict_slashes=False)
-def returnC(text):
-    """
-    C route
-
-    Arguments:
-    text: Text to be part of the url
-
-    return:
-    c + text
-    """
-    modified_text = text.replace('_', ' ')
-    return 'C.{}'.format(modified_text)
+def display_cText(text):
+    text = text.replace("_", " ")
+    return "C %s" % (text)
 
 
-if __name__ == "__main__":
-    """Making the app to run"""
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
